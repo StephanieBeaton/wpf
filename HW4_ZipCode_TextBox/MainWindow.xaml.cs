88628fef387c85af3d1655e9023cbc936341017d
Stephanie Beaton <stefanybeaton@gmail.com>
2017-03-14 13:51:28 -0700

    first commit of Homework 4 - ZipCode TextBox

---
+++ HW4_ZipCode_TextBox/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using HW4_ZipCode_TextBox.Model;
+
+namespace HW4_ZipCode_TextBox
+{
+    // ==============================================================================
+    //
+    //  CSHP 220 - C#  WPF 
+    //
+    // Homework 4: ZipCode TextBox           
+    //
+    // Create a new WPF Project with a TextBox that only accepts:
+    //
+    //   • US Zip Codes ##### or #####-####
+    //   • Canadian Postal Codes: A#B#C#
+    //
+    // The window contains a Submit button that is only enabled when a valid zip code or postal code is entered.
+    //
+    // So for example, a user could enter 98122 or 98012-4444 or T1R2X4 and the Submit button would be enabled.
+    //
+    // The Submit button does not need to perform any action.
+    //
+    // ==============================================================================
+
+
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Address myAddress = new Address();
+
+            uxGrid.DataContext = myAddress;
+        }
+
+ 
+    }
+}
---
+++ HW4_ZipCode_TextBox/Model/Address.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+using System.Collections;
+
+using System.Text.RegularExpressions;   // Regex class
+
+namespace HW4_ZipCode_TextBox.Model
+{
+
+    class Address : INotifyPropertyChanged,  IDataErrorInfo
+    {
+        private string _postalCode = string.Empty;
+        private string _postalCodeError;
+        private Boolean _submitButtonEnabled;
+
+        // Add ToString method
+        public override string ToString()
+        {
+            return _postalCode;
+        }
+
+        #region Properties
+
+        public string PostalCode
+        {
+            get
+            {
+                return _postalCode;
+            }
+
+            set
+            {
+                if (value != _postalCode)
+                {
+                    _postalCode = value;
+
+                    // trigger event 
+                    OnPropertyChanged("PostalCode");
+                }
+            }
+        }
+
+        public Boolean SubmitButtonEnabled
+        {
+            get
+            {
+                return _submitButtonEnabled;
+            }
+
+            set
+            {
+                if (value != _submitButtonEnabled)
+                {
+                    _submitButtonEnabled = value;
+
+                    // trigger event 
+                    OnPropertyChanged("SubmitButtonEnabled");
+                }
+            }
+        }
+
+        public string PostalCodeError
+        {
+            get
+            {
+                return _postalCodeError;
+            }
+            set
+            {
+                if (_postalCodeError != value)
+                {
+                    _postalCodeError = value;
+                    OnPropertyChanged("PostalCodeError");
+                }
+            }
+        }
+        #endregion
+
+
+        #region IDataErrorInfo Interface implementation
+
+        public string this[string columnName]
+        {
+            get
+            {
+                PostalCodeError = "";
+
+                switch (columnName)
+                {
+                    case "PostalCode":
+                        {
+                            //if (PostalCode != "V6N2R9")
+                            //{
+                            //    PostalCodeError = "Invalid Postal Code";
+                            //    SubmitButtonEnabled = false;
+                            //    OnPropertyChanged("SubmitButtonEnabled");
+                            //}
+                            //else
+                            //{
+                            //    SubmitButtonEnabled = true;
+                            //    OnPropertyChanged("SubmitButtonEnabled");
+                            //}
+
+                            // ===============================================
+
+                            // U.S. Postal Code
+                            string US_PostalCode_Pattern = @"^\d{5}(-\d{4}){0,1}$";
+
+                            // Canadian Postal Code
+                            string Canadian_PostalCode_Pattern = @"^[A-Z]\d[A-Z]\d[A-Z]\d$";
+
+                            // =================
+
+                            Regex findValidPostalCodesUS = new Regex(US_PostalCode_Pattern);
+
+                            var postalCodeMatchUS = findValidPostalCodesUS.Matches(PostalCode);
+
+                            // =================
+
+                            Regex findValidPostalCodesCanada = new Regex(Canadian_PostalCode_Pattern);
+
+                            var postalCodeMatchCanada = findValidPostalCodesCanada.Matches(PostalCode);
+
+                            // ================
+
+                            if (postalCodeMatchUS.Count == 0 && postalCodeMatchCanada.Count == 0)
+                            {
+                                PostalCodeError = "Invalid Postal Code";
+                                SubmitButtonEnabled = false;
+                                OnPropertyChanged("SubmitButtonEnabled");
+                            }
+                            else
+                            {
+                                SubmitButtonEnabled = true;
+                                OnPropertyChanged("SubmitButtonEnabled");
+                            }
+
+
+                            // ===============================================
+
+
+
+
+                            break;
+                        }
+                }
+
+                return PostalCodeError;
+            }
+        }
+
+
+
+        // this method is not used
+        public string Error
+        {
+            get
+            {
+                throw new NotImplementedException();
+                //return PostalCodeError;
+            }
+        }
+
+        #endregion
+
+
+        #region INotifyPropertyChanged Interface implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+    #endregion
+}
+
